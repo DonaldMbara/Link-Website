502d3079851658ae1ee4131a23b812615bbc45a6
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-08-07 11:02:35 +0200

    minimal changes

--- Link Website/js/profile.js
+++ Link Website/js/profile.js
@@ -1,21 +1,21 @@
 $(document).ready(function (){
 
       var response = '';
-      let data = window.location; //the whole link
-      data = data.toString(); //set it to string
-      let arr = data.split("username="); //split and get the link
-      let username =  arr[arr.length-1];
+      // let data = window.location; //the whole link
+      // data = data.toString(); //set it to string
+      // let arr = data.split("username="); //split and get the link
+      // let username =  arr[arr.length-1];
 
 
-      document.querySelector("#view_chats").name = username ;
-      let static_studentNumber = $('#view_chats').attr("name");
+      // document.querySelector("#view_chats").name = username ;
+      let static_studentNumber = 990916;
     $.ajax({
       header: {"Access-Control-Allow-Origin" : "*"},
       type: "GET",
       url: 'https://lamp.ms.wits.ac.za/~s1819369/profile.php',
 
       data: {
-        Username : static_studentNumber
+        StudentNo : static_studentNumber
       },
       success : function(output){
         //alert(static_studentNumber);

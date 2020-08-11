5dc58e41072889df104fcb87b194e333812eeec2
mabhutidlodlo <1428523@students.wits.ac.za>
2020-08-11 04:11:37 +0000

    Update main.js

--- Link Website/js/main.js
+++ Link Website/js/main.js
@@ -3,3 +3,14 @@ function sum(a, b) {
     return a + b;
 }
 module.exports = sum;
+
+const isPalindrome = (string) => string == string.split('').reverse().join('');
+const isAnagram = (w1, w2) => {
+    const regularize = (word) => {
+        return word.toLowerCase().split('').sort().join('').trim();
+    }
+
+    return regularize(w1) === regularize(w2);
+}
+
+module.exports = {isPalindrome, isAnagram};

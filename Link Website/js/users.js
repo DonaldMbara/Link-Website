930324e19ba04e06e53899449e8700cf6c8fa62d
mabhutidlodlo <1428523@students.wits.ac.za>
2020-08-11 04:15:07 +0000

    Update users.js

--- Link Website/js/users.js
+++ Link Website/js/users.js
@@ -1,6 +1,7 @@
-const promisify = query => new Promise((resolve, reject) => {
-  query.exec((err, data) => {
-    if (err) reject(err);
-    else resolve(data);
-  });
-});
+const returnOnError = (operation, alternative) => {
+  try {
+    return operation();
+  } catch (e) {
+    return alternative;
+  }
+};

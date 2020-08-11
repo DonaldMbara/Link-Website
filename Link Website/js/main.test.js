5bd6b7be93b1c19b59ff97874e459044c13515f7
mabhutidlodlo <1428523@students.wits.ac.za>
2020-08-11 04:22:29 +0000

    Update main.test.js

--- Link Website/js/main.test.js
+++ Link Website/js/main.test.js
@@ -1,14 +1,14 @@
-const {login, active} = require('./main'); //this imports the file i want to test
+const {isPalindrome, isAnagram,sum} = require('./main'); //this imports the file i want to test
 describe('testing string utilities', () => {
     test.each(["racecar", "radar", "level", "refer", "deified", "civic"])(
         'testing %s for palindrome', (word) => {
-            expect(login(word)).toBeTruthy();
+            expect(isPalindrome(word)).toBeTruthy();
         },
     );
 
     test.each([["arc", "car"], ["cat", "act"], ["cider", "cried"]])(
         'testing if %s and %s are anagrams ', (word1, word2) => {
-            expect(active(word1, word2)).toBeTruthy();
+            expect(isAnagram(word1, word2)).toBeTruthy();
         },
     );
 });

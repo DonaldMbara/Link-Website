536483b28f9672f1956c542af480ef99f9ae5e81
mabhutidlodlo <1428523@students.wits.ac.za>
2020-08-11 04:10:34 +0000

    Update main.test.js

--- Link Website/js/main.test.js
+++ Link Website/js/main.test.js
@@ -1,4 +1,14 @@
-const main= require('./main'); //this imports the file i want to test
-test('adds 1 + 2 to equal 3', () => {
-  expect(main(1, 2)).toBe(3);
+const {login, active} = require('./main'); //this imports the file i want to test
+describe('testing string utilities', () => {
+    test.each(["racecar", "radar", "level", "refer", "deified", "civic"])(
+        'testing %s for palindrome', (word) => {
+            expect(login(word)).toBeTruthy();
+        },
+    );
+
+    test.each([["arc", "car"], ["cat", "act"], ["cider", "cried"]])(
+        'testing if %s and %s are anagrams ', (word1, word2) => {
+            expect(active(word1, word2)).toBeTruthy();
+        },
+    );
 });

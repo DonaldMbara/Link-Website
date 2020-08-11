d67b60c58cf6347030b47b31d7c98842ecbb8274
mabhutidlodlo <1428523@students.wits.ac.za>
2020-08-11 04:55:31 +0000

    Update users.js

--- Link Website/js/users.js
+++ Link Website/js/users.js
@@ -1,7 +1,22 @@
-const returnOnError = (operation, alternative) => {
-  try {
-    return operation();
-  } catch (e) {
-    return alternative;
-  }
-};
+const sum = (vals) => {
+        
+    let sum = 0;
+    
+    vals.forEach((val) => {
+        sum += val;
+    });
+    
+    return sum;
+}
+
+const positive = (vals) => {
+
+    return vals.filter((x) => { return x > 0; });
+}
+
+const negative = (vals) => {
+
+    return vals.filter((x) => { return x < 0; });
+}
+
+module.exports = { sum, positive, negative };

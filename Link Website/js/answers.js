27a5dfd334aedfae36b9b19b2a23c5b604eadf92
mabhutidlodlo <mabhutidlodlo1@gmail.com>
2020-08-20 14:46:52 +0200

    answer and questions

---
+++ Link Website/js/answers.js
@@ -0,0 +1,153 @@
+$(document).ready(function(){
+   var courseid = localStorage.getItem("courseid");
+   var post_id = localStorage.getItem("post_id");
+   var author = localStorage.getItem("author");
+   var question = localStorage.getItem("question");
+   var date = localStorage.getItem("date");
+   var likes = localStorage.getItem("likes");
+   var userid = localStorage.getItem("userid");
+   var username = "noName";
+
+   $('.author').text(author);
+   $('.question_content').text(question);
+   $('.date').text(date);
+   $('.like_button').text(likes);
+
+//this gets all the answers to the selected question.
+   $.ajax({
+     header: {"Access-Control-Allow-Origin" :"*"},
+     type: 'GET',
+     url:'https://lamp.ms.wits.ac.za/~s1819369/Sort.php',
+     data:{post_id : post_id},
+     datatype:'json',
+     success: function (data, status){
+        var data = jQuery.parseJSON(data);
+        var source = $("#answers_template").html();
+        var template = Handlebars.compile(source);
+        $('body').append(template(data));
+
+}
+});
+//this just returns the users names.
+$.ajax({
+  header: {"Access-Control-Allow-Origin" :"*"},
+  type: 'GET',
+  url:'https://lamp.ms.wits.ac.za/~s1819369/getName.php',
+  data:{studentNo : userid},
+  datatype:'json',
+  success: function (data, status){
+      data = jQuery.parseJSON(data);
+      $.each(data, function(key, value){
+       username = value.Username;
+   });
+
+}
+});
+
+//this likes the question and decrease or increase the likes.
+$('body').on('click', '.like_button',function(e) {
+  $.ajax({
+    header: {"Access-Control-Allow-Origin" :"*"},
+    datatype:'json',
+    type: "POST",
+    url: 'https://lamp.ms.wits.ac.za/~s1819369/Testing.php',
+    data:{id:post_id, userid:userid},
+    success: function (data, status) {
+        $(this).val(data.likes);
+        window.location.reload(true);
+
+       }
+     });
+});
+
+//this takes inn the answer like minus or add likes.
+$('body').on('click', '#like_button',function(e) {
+
+  id = $(this).parents()[0].id ;
+  $.ajax({
+    header: {"Access-Control-Allow-Origin" :"*"},
+    datatype:'json',
+    type: "POST",
+    url: 'https://lamp.ms.wits.ac.za/~s1819369/Testing2.php',
+    data:{id:id, userid:userid},
+    success: function (data, status) {
+        $(this).val(data.likes);
+        window.location.reload(true);
+
+       }
+     });
+
+});
+
+//when you press the answer you view or hide comments
+$('body').on('click', '#answer_content', function(){
+     var answer_id = $(this).parents()[0].id;
+
+     var id = $(this).parents()[1];
+    if($(id).find('#comment-box').hasClass('hidden')){
+      $.ajax({
+        header: {"Access-Control-Allow-Origin" :"*"},
+        type: 'GET',
+        url:'https://lamp.ms.wits.ac.za/~s1819369/comments.php',
+        data:{post_id :answer_id},
+        success: function (data, status){
+            data = jQuery.parseJSON(data);
+          var output = '<div class="container">';
+          $.each(data, function(key, value){
+             output += '<h5>' + value.author + '</h5>';
+             output += '<p id="comment_p">' + value.comment + '</p>'
+                });
+
+          output += '</div>';
+          $(id).find('#comments_holder').html(output);
+
+          $(id).find('#comment-box').removeClass('hidden');
+
+      }
+      });
+
+    }
+   else{
+     $(id).find('#comment-box').addClass('hidden');
+   }
+
+});
+
+//this executes after you pressed the comment button, and uploads the comment
+$('body').on('click', '#comment_button', function(e){
+    id = $(this).parents()[0];
+    id2 = $(this).parents()[1];
+    answer_id = $(id2).find('.answer_card').attr('id');
+    comment = $(id).find('.comment_input').val();
+    $.ajax({
+      header: {"Access-Control-Allow-Origin" :"*"},
+      datatype:'json',
+      type: "POST",
+      url: 'https://lamp.ms.wits.ac.za/~s1819369/comments.php',
+      data:{post_id:answer_id, author:username, comment:comment},
+      success: function (data, status) {
+          window.location.reload(true);
+
+         }
+       });
+});
+
+//this takes the answer and post it then reloaads the page
+$('body').on('click', '#answer_button', function(e){
+    parent = $(this).parents()[0];
+    answer = $("#"+parent.id).find('#answer_input').val();
+    author = "mj";
+    $.ajax({
+      header: {"Access-Control-Allow-Origin" :"*"},
+      datatype:'json',
+      type: "POST",
+      url: 'https://lamp.ms.wits.ac.za/~s1819369/addAns.php',
+      data:{author:username, post_id:post_id, answer:answer},
+      success: function (data, status) {
+          window.location.reload(true);
+
+         }
+       });
+});
+
+});
---
+++ Link Website/js/questions.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    var courseid= 1;
+    var querySet;
+    var userid = 12345678;
+
+    //this loads all the questions from the selected course.
+    $.ajax({
+       header: {"Access-Control-Allow-Origin" :"*"},
+       type: 'GET',
+       url:'https://lamp.ms.wits.ac.za/~s1819369/getposts.php',
+       data:{courseid:courseid},
+       datatype:'json',
+       success: function (data, status){
+
+          var data = jQuery.parseJSON(data);
+          querySet = data;
+          var source = $("#questions_template").html();
+          var template = Handlebars.compile(source);
+          $('body').append(template(data));
+
+           }
+
+    });
+
+//this is the live question filter.
+    $('#txt-search').on("keyup input", function(){
+       var inputVal = $(this).val();
+       if(inputVal.length){
+
+          $.ajax({
+
+            header: {"Access-Control-Allow-Origin" :"*"},
+            datatype:'json',
+            type:'GET',
+            url:"https://lamp.ms.wits.ac.za/~s1819369/live.php",
+            data:{term: inputVal,courseid: courseid},success:(function(data){
+
+               var data = jQuery.parseJSON(data);
+               var output = '<div class="container">';
+               $.each(data, function(key, value){
+        		       output += '<h5>' + value.POST_AUTHOR + '</h5>';
+        		       output += '<p id="question_p">' + value.POST_STATUS + '</p>'
+
+                   });
+
+             output += '</div>';
+             $('#results').html(output);
+
+                })
+
+             });
+
+         }
+
+     });
+
+//this the question like.
+  $('body').on('click','i',function(e){
+    var id  = $(this).parent();
+    var post_id = id[0].id;
+    $.ajax({
+      header: {"Access-Control-Allow-Origin" :"*"},
+      datatype:'json',
+      type: "POST",
+      url: 'https://lamp.ms.wits.ac.za/~s1819369/Testing.php',
+      data:{id:post_id, userid:userid},
+      success: function (data, status) {
+          $(this).val(data.likes);
+          window.location.reload(true);
+
+         }
+       });
+
+   });
+
+
+//this takes the question you will search to the top of the page.
+  $('#results').on('click', 'p', function() {
+
+     var click_text = $(this).text().split('|');
+     $('#txt-search').val($.trim(click_text[0]));
+     $("#results").html('');
+     var question_id;
+     $.each(querySet, function(key, value){
+        if(value.POST_STATUS == $("#txt-search").val()){
+           question_id = value.POST_ID;
+
+           }
+       });
+
+      var el_up = document.getElementById("question_id");
+        window.scrollTo(0, $("#"+question_id).offset().top);
+    });
+
+// this takes the required data and loads the answers html
+    $('body').on('click', '.question_card', function() {
+
+       n = $(this).contents();
+       var post_id = (this.id);
+       localStorage.setItem("post_id",post_id);
+       localStorage.setItem("author",$(this).find("#author").text());
+       localStorage.setItem("question",$(this).find("#question_content").text());
+       localStorage.setItem("date",$(this).find("#date").text());
+       localStorage.setItem("likes",$(this).find("#like_button").text());
+       localStorage.setItem("userid",userid);
+        window.location.href = "answers.html";
+
+
+        });
+
+});

3de055d6942412759c5b6d1be748cb01c803f296
BL4CKDUDE <61325361+BL4CKDUDE@users.noreply.github.com>
2020-08-21 18:52:02 +0200

    updated events

--- Link Website/js/eventUsers.js
+++ Link Website/js/eventUsers.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+  var querySet;
     $('#table tr').click(function() {
         $('#user_chat').html('dtfyguh');
 
@@ -13,19 +13,106 @@ $(document).ready(function () {
 
             $.each(data, function (key, value) {
 
-                username += '<div class="w3-container w3-left-align w3-padding-10 " style="background-color: lightyellow;">'+
+                username +=
                     '<tr>' ;
-                username += '<td ">' + ' <a href="#" ><li>'+
-                    value.Username  + '</li></a> '+'</td>' ;
-                username +=  '</tr>' + '</div>';
+                username += '<td >' +
+                    value.Username  +'</td>';
+                username += '<td>' + '<button class="ebtn">' + 'View</button>' + '</td>';
+                username +=  '</tr>';
             });
 
             //INSERTING ROWS INTO TABLE
             $('#table').append(username);
         });
 
+        let name = "Nobody";
+
+
+
+        $('#table').on('click', '.ebtn', function(){
+          // alert("Marcel");
+          $("div.card").remove();
+          var row= $(this).closest("tr");
+          var name = row.find("td:eq(0)").text();
+          // window.location("events.html");
+          getEvent(name);
+        });
+
+        // getEvent("Marcel");
+
+
+        function getEvent(name){
+
+        $.ajax({
+           header: {"Access-Control-Allow-Origin" :"*"},
+           type: 'GET',
+           url:'https://lamp.ms.wits.ac.za/~s1819369/getevent.php',
+           data:{fname:name},
+           datatype:'json',
+           success: function (data, status){
+
+              var data = jQuery.parseJSON(data);
+
+              for(var i =0; i< data.length; i++){
+                if(data[i].Availability == 1){
+                  // alert(data[0].Availability);
+                    data[i].Availability = "Available";
+                }
+              }
+
+              for(var i =0; i< data.length; i++){
+                if(data[i].Month == 0){
+                  data[i].Month = "January";
+                }
+                else if(data[i].Month == 1){
+                  data[i].Month = "February";
+                }
+                else if(data[i].Month == 2){
+                  data[i].Month = "March";
+                }
+                else if(data[i].Month == 3){
+                  data[i].Month = "April";
+                }
+                else if(data[i].Month == 4){
+                  data[i].Month = "May";
+                }
+                else if(data[i].Month == 5){
+                  data[i].Month = "June";
+                }
+                else if(data[i].Month == 6){
+                  data[i].Month = "July";
+                }
+                else if(data[i].Month == 7){
+                  data[i].Month = "August";
+                }
+                else if(data[i].Month == 8){
+                  data[i].Month = "September";
+                }
+                else if(data[i].Month == 9){
+                  data[i].Month = "October";
+                }
+                else if(data[i].Month == 10){
+                  data[i].Month = "November";
+                }
+                else if(data[i].Month == 11){
+                  data[i].Month = "December";
+                }
+              }
+
+              querySet = data;
+              var source = $("#events_template").html();
+              var template = Handlebars.compile(source);
+              $('body').append(template(data));
+
+               }
+
+        });
+      }
 
 
 
 
-})
+
+
+
+});

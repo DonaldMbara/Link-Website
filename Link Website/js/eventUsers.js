44993379376165719dae0756eabff25a0b09424b
BL4CKDUDE <61325361+BL4CKDUDE@users.noreply.github.com>
2020-08-21 11:15:43 +0200

    Updated Events

---
+++ Link Website/js/eventUsers.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('#table tr').click(function() {
+        $('#user_chat').html('dtfyguh');
+
+    });
+
+
+    $.getJSON("https://lamp.ms.wits.ac.za/~s1819369/getNames.php",
+
+        function (data) {
+            var username = '';
+
+            $.each(data, function (key, value) {
+
+                username += '<div class="w3-container w3-left-align w3-padding-10 " style="background-color: lightyellow;">'+
+                    '<tr>' ;
+                username += '<td ">' + ' <a href="#" ><li>'+
+                    value.Username  + '</li></a> '+'</td>' ;
+                username +=  '</tr>' + '</div>';
+            });
+
+            //INSERTING ROWS INTO TABLE
+            $('#table').append(username);
+        });
+
+
+
+
+
+})

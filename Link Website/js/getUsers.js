0f688bd999044b07502c891e6bb8a55d6c5884c1
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-08-21 22:08:59 +0200

    chat

--- Link Website/js/getUsers.js
+++ Link Website/js/getUsers.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
             var username = '';
 
             $.each(data, function (key, value) {
-                let name = value.Username;
+
                 username += '<div class="w3-container w3-left-align w3-padding-10 " style="background-color: lightyellow;">'+
                     '<tr>' ;
-                username += '<td ">' + ' <a href="#" onclick="getName(name)" ><li>'+
+                username += '<td ">' + ' <a href="#" onclick="alert(1)" ><li>'+
                     value.Username  + '</li></a> '+'</td>' ;
                 username +=  '</tr>' + '</div>';
             });
@@ -22,7 +22,44 @@ $(document).ready(function () {
             $('#table').append(username);
         });
 
+    //this executes after you pressed the send button, and uploads the send
+    $('body').on('click', '#comment_button', function(e){
+        id = $(this).parents()[0];
+        id2 = $(this).parents()[1];
+        answer_id = $(id2).find('.message_card').attr('id');
+        comment = $(id).find('.message_input').val();
+        $.ajax({
+            header: {"Access-Control-Allow-Origin" :"*"},
+            datatype:'json',
+            type: "POST",
+            url: 'https://lamp.ms.wits.ac.za/~s1819369/insertMessage.php',
+            data:{message:answer,sender:username,receiver:"dddd", rStudNum:1111,sStudNum:12323},
+            success: function (data, status) {
+                window.location.reload(true);
 
+            }
+        });
+    });
+
+
+
+    //this takes the message and post it then reloaads the page
+    $('body').on('click', '#send_message', function(e){
+        parent = $(this).parents()[0];
+        answer = $("#"+parent.id).find('#message_input').val();
+        author = "mj";
+        $.ajax({
+            header: {"Access-Control-Allow-Origin" :"*"},
+            datatype:'json',
+            type: "POST",
+            url: 'https://lamp.ms.wits.ac.za/~s1819369/getMessages.php',
+            data:{message:answer,sender:username,receiver:"dddd", rStudNum:1111,sStudNum:12323},
+            success: function (data, status) {
+                window.location.reload(true);
+
+            }
+        });
+    });
 
 
 })

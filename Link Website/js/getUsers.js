285aca7c6742527772c70273fed603275957d529
sweetmoh <61325364+sweetmoh@users.noreply.github.com>
2020-08-22 11:58:59 +0200

    added message view and send button

--- Link Website/js/getUsers.js
+++ Link Website/js/getUsers.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
 
 
 
-
-
     $.getJSON("https://lamp.ms.wits.ac.za/~s1819369/getNames.php",
 
         function (data) {
@@ -11,9 +9,8 @@ $(document).ready(function () {
 
             $.each(data, function (key, value) {
 
-                username += '<div class="w3-container w3-left-align w3-padding-10 " style="background-color: lightyellow;">'+
-                    '<tr>' ;
-                username += '<td ">' + ' <a href="#" onclick="alert(1)" ><li>'+
+                username += '<div class="w3-container w3-left-align w3-padding-10 " style="background-color: lightyellow;">'+ '<tr>' ;
+                username += '<td>' + ' <a href="chat_messages.html" ><li>'+
                     value.Username  + '</li></a> '+'</td>' ;
                 username +=  '</tr>' + '</div>';
             });
@@ -21,47 +18,4 @@ $(document).ready(function () {
             //INSERTING ROWS INTO TABLE
             $('#table').append(username);
         });
-
-    //this executes after you pressed the send button, and uploads the send
-    $('body').on('click', '#comment_button', function(e){
-        id = $(this).parents()[0];
-        id2 = $(this).parents()[1];
-        answer_id = $(id2).find('.message_card').attr('id');
-        comment = $(id).find('.message_input').val();
-        $.ajax({
-            header: {"Access-Control-Allow-Origin" :"*"},
-            datatype:'json',
-            type: "POST",
-            url: 'https://lamp.ms.wits.ac.za/~s1819369/insertMessage.php',
-            data:{message:answer,sender:username,receiver:"dddd", rStudNum:1111,sStudNum:12323},
-            success: function (data, status) {
-                window.location.reload(true);
-
-            }
-        });
-    });
-
-
-
-    //this takes the message and post it then reloaads the page
-    $('body').on('click', '#send_message', function(e){
-        parent = $(this).parents()[0];
-        answer = $("#"+parent.id).find('#message_input').val();
-        author = "mj";
-        $.ajax({
-            header: {"Access-Control-Allow-Origin" :"*"},
-            datatype:'json',
-            type: "POST",
-            url: 'https://lamp.ms.wits.ac.za/~s1819369/getMessages.php',
-            data:{message:answer,sender:username,receiver:"dddd", rStudNum:1111,sStudNum:12323},
-            success: function (data, status) {
-                window.location.reload(true);
-
-            }
-        });
-    });
-
-
-})
-
-
+});

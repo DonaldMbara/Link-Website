60847b51ca07ef6837505431be02c06422f85411
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-08-31 21:47:13 +0200

    able to send message with name from selected list
    left with studentNo

--- Link Website/js/getUsers.js
+++ Link Website/js/getUsers.js
@@ -1,26 +1,14 @@
 $(document).ready(function () {
 
 
-    let arrayName = []
-
-    $.getJSON("https://lamp.ms.wits.ac.za/~s1819369/getNames.php",
-
-        function (data) {
-            var username = '';
-
-            $.each(data, function (key, value) {
-                me = value;
-
-                username += '<div  style="background-color: lightyellow; padding-left:10px ;">'+ '<tr>' ;
-                username += '<td>' + ' <button style="width: 250px;"  onclick="alert(1)" ><li>'+
-                    value.Username  + '</li></button> '+'</td>' ;
-                username +=  '</tr>' + '</div>';
 
 
 
-            });
 
-            //INSERTING ROWS INTO TABLE
-            $('#table').append(username);
-        });
+    // getEvent("Marcel");
+
+
+
+
+
 });
--- Link Website/js/sendMessages.js
+++ Link Website/js/sendMessages.js
@@ -1,12 +1,49 @@
 $(document).ready(function () {
+
+    $.getJSON("https://lamp.ms.wits.ac.za/~s1819369/getNames.php",
+
+        function (data) {
+            var username = '';
+
+            $.each(data, function (key, value) {
+
+
+                username +=
+                    '<tr>' ;
+                username += '<td >' +
+                    value.Username  +'</td>';
+                username += '<td>' + '<button class="ebtn">' + 'View</button>' + '</td>';
+                username +=  '</tr>';
+            });
+
+            //INSERTING ROWS INTO TABLE
+            $('#table').append(username);
+        });
+
+    $('#table').on('click', '.ebtn', function(){
+        //alert("Marcel");
+        var row= $(this).closest("tr");
+        var name = row.find("td:eq(0)").text();
+        // window.location("events.html");
+        document.getElementById("recName").innerHTML = name; //set username
+        localStorage.setItem("uName",name);
+        // alert(name);
+    });
+
+
     $('#sending_message').submit(function (e) {
         e.preventDefault();
 
+
+
+
         let sender_name = localStorage.getItem("username");
-        let receiver_name = '';
+        let receiver_name = localStorage.getItem("uName");
         let receiver_stud = '';
         let sender_stud =  localStorage.getItem("key");
         let message = document.getElementById("message_input").value;
+
+
 
 
 
@@ -17,27 +54,31 @@ $(document).ready(function () {
         }else {
 
 
-            $.ajax({
-                header: {"Access-Control-Allow-Origin": "*"},
-                type: "POST",
-                url: 'https://lamp.ms.wits.ac.za/~s1819369/insertMessage.php',
-                data: {
-                    message: message,
-                    sender: sender_name,
-                    receiver: receiver_name,
-                    rStudNum: receiver_stud,
-                    sStudNum: sender_stud
 
-                },
-                success: function (data) {
-                    $('#message_input').val("");//clear
-                },
-            });
+                $.ajax({
+                    header: {"Access-Control-Allow-Origin": "*"},
+                    type: "POST",
+                    url: 'https://lamp.ms.wits.ac.za/~s1819369/insertMessage.php',
+                    data: {
+                        message: message,
+                        sender: sender_name,
+                        receiver: receiver_name,
+                        rStudNum: receiver_stud,
+                        sStudNum: sender_stud
+
+                    },
+                    success: function (data) {
+                        $('#message_input').val("");//clear
+                    },
+                });
+
+
+
 
         }
 
-
-
-
     });
+
+
+
 });

f391ce68d4a986c19ddb72e886a7eaa2d6f9e296
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-09-07 22:53:15 +0200

    Update sendMessage.test.js

--- Link Website/js/sendMessage.test.js
+++ Link Website/js/sendMessage.test.js
@@ -1,35 +1,35 @@
-// jest
-//   .dontMock('fs')
-//   .dontMock('jquery');
-//
-// var $ = require('jquery');
-// window.$ = $;
-// var fs = require('fs');
-// var jqueryT = fs.readFileSync('Link Website/js/sendMessages.js'); //this imports the file i want to test
-//
-// var html = fs.readFileSync('Link Website/html/chat_messages.html','utf-8');
-// describe('validateSubmits', function() {
-//   it('shows/hides error banner', function() {
-//      question = require('./sendMessages.js'); //this imports the file i want to test
-//     const scriptEl = window.document.createElement('script');
-//     scriptEl.textContent = jqueryT;
-//     window.document.body.appendChild(scriptEl);
-//
-//     $.ajax = jest.fn().mockImplementation(question => {
-//       let instance = {
-//         done: fn => {
-//           if (question.success) fn("test passed");
-//           return instance;
-//         },
-//         fail: fn => {
-//           if (!question.success) fn("test failed");
-//           return instance;
-//         }
-//       };
-//       return instance;
-//     });
-//
-//
-//
-// });
-// });
+jest
+  .dontMock('fs')
+  .dontMock('jquery');
+
+var $ = require('jquery');
+window.$ = $;
+var fs = require('fs');
+var jqueryT = fs.readFileSync('Link Website/js/sendMessages.js'); //this imports the file i want to test
+
+var html = fs.readFileSync('Link Website/html/chat_messages.html','utf-8');
+describe('validateSubmits', function() {
+  it('shows/hides error banner', function() {
+     question = require('./sendMessages.js'); //this imports the file i want to test
+    const scriptEl = window.document.createElement('script');
+    scriptEl.textContent = jqueryT;
+    window.document.body.appendChild(scriptEl);
+
+    $.ajax = jest.fn().mockImplementation(question => {
+      let instance = {
+        done: fn => {
+          if (question.success) fn("test passed");
+          return instance;
+        },
+        fail: fn => {
+          if (!question.success) fn("test failed");
+          return instance;
+        }
+      };
+      return instance;
+    });
+
+
+
+});
+});

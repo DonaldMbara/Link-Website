963e9f0f13a3368378a7a17ef7387a0b2ab17db3
mabhutidlodlo <1428523@students.wits.ac.za>
2020-09-15 08:34:47 +0000

    Update fetchMessage.js

--- Link Website/js/fetchMessage.js
+++ Link Website/js/fetchMessage.js
@@ -25,24 +25,24 @@ function getDetails(receiver_stud,sender_stud) {
 }
 
 
-// const sum = (vals) => {
-//
-//     let sum = 0;
-//
-//     vals.forEach((val) => {
-//         sum += val;
-//     });
-//
-//     return sum;
-// }
-//
-// const positive = (vals) => {
-//
-//     return vals.filter((x) => { return x > 0; });
-// }
-//
-// const negative = (vals) => {
-//
-//     return vals.filter((x) => { return x < 0; });
-// }
-// module.exports = { sum, positive, negative};
+ const sum = (vals) => {
+
+     let sum = 0;
+
+     vals.forEach((val) => {
+         sum += val;
+     });
+
+     return sum;
+ }
+
+ const positive = (vals) => {
+
+     return vals.filter((x) => { return x > 0; });
+ }
+
+ const negative = (vals) => {
+
+     return vals.filter((x) => { return x < 0; });
+ }
+ module.exports = { sum, positive, negative};

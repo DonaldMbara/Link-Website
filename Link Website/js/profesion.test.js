a933da43e7799a5bc5307c3a518524f601cb9ee2
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-10-03 21:42:30 +0200

    Update profesion.test.js

--- Link Website/js/profesion.test.js
+++ Link Website/js/profesion.test.js
@@ -1,67 +1,44 @@
-
 jest
-  .dontMock('fs')
-  .dontMock('jquery');
+    .dontMock('fs')
+    .dontMock('jquery');
 
 var $ = require('jquery');
 window.$ = $;
+var fs = require('fs');
+var jqueryT = fs.readFileSync('Link Website/js/profession.js'); //this imports the file i want to test
+
+var html = fs.readFileSync('Link Website/html/profession.html','utf-8');
+describe('validateSubmits', function() {
+    it('shows/hides error banner', function() {
+        question = require('./profession.js'); //this imports the file i want to test
+
+    });
+});
+
 
 describe('vals', function(){
-const { sum, positive, negative, s1,p1,n1,s2,p2,n2 } = require('./profession');
-let vals;
-let sum_of_vals;
-let pos_vals;
-let neg_vals;
+    const { sum, positive, negative } = require('./profession');
+    let vals;
+    let sum_of_vals;
+    let pos_vals;
+    let neg_vals;
 
-beforeAll(() => {
+    beforeAll(() => {
+        pos_vals = [2, 1, 3];
+        neg_vals = [-2, -1, -1];
+        vals = pos_vals.concat(neg_vals);
+        sum_of_vals = vals.reduce((x, y) => x + y, 0);
+    })
 
-    pos_vals = [2, 1, 3];
-    neg_vals = [-2, -1, -1];
-    vals = pos_vals.concat(neg_vals);
-    sum_of_vals = vals.reduce((x, y) => x + y, 0);
-})
+    test('the sum of vals should be 2', () => {
+        expect(sum(vals)).toBe(sum_of_vals);
+    });
 
-test('the sum of vals should be 2', () => {
-    expect(sum(vals)).toBe(sum_of_vals);
-});
+    test('should get positive values', () => {
+        expect(positive(vals)).toEqual(pos_vals);
+    });
 
-test('should get positive values', () => {
-    expect(positive(vals)).toEqual(pos_vals);
-});
-
-test('should get negative values', () => {
-    expect(negative(vals)).toEqual(neg_vals);
-});
-test('the sum of vals should be 2', () => {
-    expect(s1(vals)).toBe(sum_of_vals);
-});
-
-test('should get positive values', () => {
-    expect(p1(vals)).toEqual(pos_vals);
-});
-
-test('should get negative values', () => {
-    expect(n1(vals)).toEqual(neg_vals);
-});
-
-
-beforeAll(() => {
-
-    pos_vals = [2, 1, 3];
-    neg_vals = [-2, -1, -1];
-    vals = pos_vals.concat(neg_vals);
-    sum_of_vals = vals.reduce((x, y) => x + y, 0);
-})
-
-test('the sum of vals should be 2', () => {
-    expect(s2(vals)).toBe(sum_of_vals);
-});
-
-test('should get positive values', () => {
-    expect(p2(vals)).toEqual(pos_vals);
-});
-
-test('should get negative values', () => {
-    expect(n2(vals)).toEqual(neg_vals);
-});
+    test('should get negative values', () => {
+        expect(negative(vals)).toEqual(neg_vals);
+    });
 });

8c4cb2f55f6a4bc717462c1e9fdc6cbbb0eadd99
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-10-05 00:42:26 +0200

    test

--- Link Website/js/uploadPic.js
+++ Link Website/js/uploadPic.js
@@ -1,34 +1,34 @@
 
-//Adding a submit function to the form
-$('#upload').submit(function(e){
-
-     e.preventDefault();
-
-
-
-
-    $.ajax({
-
-        url: "https://lamp.ms.wits.ac.za/~s1819369/profpic.php",
-
-        //For file upload we use post request
-        type: "POST",
-
-        //Creating data from form
-        data: new FormData(this),
-
-        //Setting these to false because we are sending a multipart request
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function(data){
-            //If the request is successfull we will get the scripts output in data variable
-            //Showing the result in our html element
-            alert(data);
-        },
-        error: function(){}
-    });
-});
+// //Adding a submit function to the form
+// $('#upload').submit(function(e){
+//
+//      e.preventDefault();
+//
+//
+//
+//
+//     $.ajax({
+//
+//         url: "https://lamp.ms.wits.ac.za/~s1819369/profpic.php",
+//
+//         //For file upload we use post request
+//         type: "POST",
+//
+//         //Creating data from form
+//         data: new FormData(this),
+//
+//         //Setting these to false because we are sending a multipart request
+//         contentType: false,
+//         cache: false,
+//         processData: false,
+//         success: function(data){
+//             //If the request is successfull we will get the scripts output in data variable
+//             //Showing the result in our html element
+//             alert(data);
+//         },
+//         error: function(){}
+//     });
+// });
 
 
 const sum = (vals) => {

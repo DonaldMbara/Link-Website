8cb437267af1f808afaaa980ade3565b49383e70
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-10-10 21:36:51 +0200

    testing

--- Link Website/js/profesion.test.js
+++ Link Website/js/profesion.test.js
@@ -1,25 +1,7 @@
-const { sum, positive, negative } = require('./users');
+const uppercase = require('./uppercase')
 
-let vals;
-let sum_of_vals;
-let pos_vals;
-let neg_vals;
-
-beforeAll(() => {
-    pos_vals = [2, 1, 3];
-    neg_vals = [-2, -1, -1];
-    vals = pos_vals.concat(neg_vals);
-    sum_of_vals = vals.reduce((x, y) => x + y, 0);
+test(`uppercase 'test' to equal 'TEST'`, () => {
+    return uppercase('').catch(e => {
+        expect(e).toMatch('Empty string')
+    })
 })
-
-test('the sum of vals should be 2', () => {
-    expect(sum(vals)).toBe(sum_of_vals);
-});
-
-test('should get positive values', () => {
-    expect(positive(vals)).toEqual(pos_vals);
-});
-
-test('should get negative values', () => {
-    expect(negative(vals)).toEqual(neg_vals);
-});
--- Link Website/js/profession.js
+++ Link Website/js/profession.js
@@ -1,22 +1,11 @@
-const sum = (vals) => {
-
-    let sum = 0;
-
-    vals.forEach((val) => {
-        sum += val;
-    });
-
-    return sum;
+const uppercase = str => {
+return new Promise((resolve, reject) => {
+    if (!str) {
+        reject('Empty string')
+        return
+    }
+    resolve(str.toUpperCase())
+})
 }
 
-const positive = (vals) => {
-
-    return vals.filter((x) => { return x > 0; });
-}
-
-const negative = (vals) => {
-
-    return vals.filter((x) => { return x < 0; });
-}
-
-module.exports = { sum, positive, negative };
+module.exports = uppercase
